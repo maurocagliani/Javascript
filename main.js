a4ac6b2c27e258b1286938b6ba9b95e2e2f47f76
maurocagliani <111257147+maurocagliani@users.noreply.github.com>
2023-04-20 19:33:23 -0300

    Modificado

--- main.js
+++ main.js
@@ -19,11 +19,6 @@ let pilotuser = prompt("Ingrese piloto si es piloto o alumno si es alumno piloto
 let cantidadTurnos = 0;
 let avion = "avion";
 let dia = "domingo";
-<<<<<<< HEAD
-
-=======
-//s
->>>>>>> 81b81e5 (Preentregas Javascript)
 
 //Elegir tipo de avion
 function elegirAvion(avion){
