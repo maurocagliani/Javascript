9c8764e9f7641bd3de5a8b79b06d0634d51e12b9
maurocagliani <111257147+maurocagliani@users.noreply.github.com>
2023-05-02 19:10:13 -0300

    Segunda entrega

--- main.js
+++ main.js
@@ -1,63 +1,100 @@
-// precio turno
+// precio turno - Lo voy a implementar en la siguiente entrega.
 const
     precioalumno = 70;
     preciopiloto = 60;
 
 
-function piloto(nombreUsuario) {
-    alert("Hola " + nombreUsuario + " Ingresaste como piloto");
+
+class Avion {
+  constructor(id, marca, modelo) {
+    this.id = id;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.lunes = [true, true, true, true, true];
+    this.martes = [true, true, true, true, true];
+    this.miercoles = [true, true, true, true, true];
+    this.jueves = [true, true, true, true, true];
+    this.viernes = [true, true, true, true, true];
+  }
 }
 
-function alumnopiloto(nombreUsuario) {
-    alert("Hola " + nombreUsuario + " Ingresaste como alumno");
+class Usuario {
+  constructor(nombre, email) {
+    this.nombre = nombre;
+    this.email = email;
+    this.reservas = [];
+  }
+
+  agregarReserva(reserva, turno) {
+    this.reservas.push(reserva);
+    this.reservas.push(turno);
+  }
 }
 
-
-//Variables del programa principal
-let usuario = prompt("Ingrese Su nombre");
-let pilotuser = prompt("Ingrese piloto si es piloto o alumno si es alumno piloto");
-let cantidadTurnos = 0;
-let avion = "avion";
-let dia = "domingo";
-
-//Elegir tipo de avion
-function elegirAvion(avion){
-    alert('Los aviones disponibles son Cessna 150 y Tecnam P2002');
-    avion = prompt("¿Con què aviòn vas a solicitar turno? ");
-    return avion;
+function diaElegido(dia) {
+  dia = prompt('Elija un día para reservar un turno: (Domingo para salir)');
+  return dia;
 }
 
-//Elegir el dìa
+const avionesDisponibles = [
+  new Avion(0, "Cessna", "150"),
+  new Avion(1, "Piper", "Arrow II"),
+];
 
-function diaReserva(dia){
-    dia = prompt("¿Què dìa vas a solicitar el/los turnos? ");
-    return dia;
+let usuario = new Usuario();
+usuario.nombre = prompt('Ingrese su nombre: ');
+usuario.email = prompt('Ingrese su eMail: ');
+
+alert('El avión 0 es Cessna 150 y el Avion 1 Piper Arrow II');
+let idAvion = prompt('¿Con qué avión vas a reservar turno? ');
+
+let avionElegido = avionesDisponibles[idAvion];
+
+const hoy = new Date();
+
+alert(
+  'Para el avión ' + avionElegido.marca + ' existen los siguientes turnos: '
+);
+
+['lunes', 'martes', 'miercoles', 'jueves', 'viernes'].forEach((dia) => {
+  console.log(`Para el día ${dia}:`);
+  avionElegido[dia].map((disponible, index) => {
+    console.log(`  El turno ${index + 1} está ${disponible ? 'disponible' : 'ocupado'}`);
+  });
+});
+
+let diaReserva = diaElegido();
+while (diaReserva.toLowerCase() != 'domingo') {
+  let turnoElegido = Number(prompt('Elijar un turno para la reserva: '));
+  turnoElegido = turnoElegido - 1;
+  if (avionElegido[diaReserva][turnoElegido] == true) {
+    avionElegido[diaReserva][turnoElegido] = false;
+    usuario.agregarReserva({ diaReserva, turnoElegido, hoy });
+    alert(`Tu turno es el día ${diaReserva} con turno ${turnoElegido + 1}`);
+  } else {
+    alert('Ese turno no está disponible. Por favor, elija otro turno del 1 al 5');
+  }
+  diaReserva = diaElegido();
 }
 
-if (pilotuser == "piloto"){
-    piloto(usuario);
+alert(
+  'Para el avión ' + avionElegido.marca + ' existen los siguientes turnos: '
+);
+
+['lunes', 'martes', 'miercoles', 'jueves', 'viernes'].forEach((dia) => {
+  console.log(`Para el día ${dia}:`);
+  avionElegido[dia].map((disponible, index) => {
+    console.log(`  El turno ${index+1} está ${disponible ? "Disponible" : "No disponible"}`);
+  });
+});
+
+console.log('Reservas del usuario:');
+
+for (let i = 0; i < usuario.reservas.length; i += 2) {
+  const reserva = usuario.reservas[i];
+  const turno = usuario.reservas[i + 1];
+  console.log(
+    `  - Reservaste el turno ${turno + 1} del día ${reserva.diaReserva}. Reservaste el dìa ${reserva.hoy.toLocaleDateString()}`
+  );
 }
-else {
-    alumnopiloto(usuario);
-}
-
-//programa principal
-let avionElegido = elegirAvion(avion);
-
-let diaElegido = diaReserva(dia);
-
-
-
-let reservarTurno= Number(prompt("Ingrese cantidad de turnos a reservar: "));
-
-//si ingresa -1 no entra al while y cancela la reserva de turno
-while (reservarTurno != -1) {
-    cantidadTurnos = cantidadTurnos + reservarTurno;
-    reservarTurno= Number(prompt("Ingrese cantidad de turnos a reservar: "));
-
-}
-let precioTotal = cantidadTurnos * precioalumno;
-alert("Reservaste " + cantidadTurnos + " turnos.");
-alert("El valor de la reserva es de " + precioTotal + " dolares el dìa " + diaElegido + " con el avion " + avionElegido);
-
 
