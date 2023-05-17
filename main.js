9c969bbad6c995cd3b0f16c84d72823b97c5db5b
maurocagliani <111257147+maurocagliani@users.noreply.github.com>
2023-05-16 23:01:47 -0300

    Segunda entrega con clases

--- main.js
+++ main.js
@@ -98,3 +98,71 @@ for (let i = 0; i < usuario.reservas.length; i += 2) {
   );
 }
 
+//agrego el hijo el nodo creado
+
+// //Se le puede decir en que parte del body?
+// [Tutora] Diana Mottura 20:43
+// sí!
+// Ángel León 20:43
+// como?
+// [Tutora] Diana Mottura 20:44
+// podés obtener con getElement a qué lugar del html querés agregarlo
+// estaba escribiendo jaja
+// por ej
+// Ángel León 20:44
+// @[Tutora] Diana Mottura ah jajaj perdón la ansiedad
+// [Tutora] Diana Mottura 20:44
+// const contenedor = document.getElementById("contenedor")
+
+// contenedor.appendChild(elemento)
+
+// let parrafo = document.createElement("p");
+// parrafo.innerHTML = "<p>Seleccionà el dìa y turno para reservar un turno</p>";
+
+// document.body.append(parrafo);
+
+let parrafo = document.createElement("p");
+parrafo.textContent = "Selecciona el día y turno para reservar un turno";
+document.body.appendChild(parrafo);
+let producto = {
+  id: 1,
+  nombre: "Patagonia",
+  precio: 650
+};
+
+let plantillaNueva = document.createElement("p");
+// alt+96
+plantillaNueva.innerHTML = `
+ID: ${producto.id} - Producto: ${producto.nombre} - Precio: ${producto.precio};
+`;
+
+document.main.appendChild(plantillaNueva);
+
+
+//Eventos con nombre de evento y funcion de retorno como parametro
+
+// let botonPrincipal = document.getElementById(btnPrincipal);
+// botonPrincipal.addEventListener('click', respuestaClick)
+//   function respuestaClick(){
+//     console.log("respuesta1");
+// }
+
+//========= opcion 2: Emplear propiedad del NODO
+
+let botonPrincipal = document.getElementById(btnPrincipal);
+botonPrincipal.onclick = () => {
+  console.log("respuesta 2");
+}
+ 
+
+//Obtenemos el nodo donde vamos a agregar los nuevos elementos
+let padre = document.getElementById("personas");
+//Array con la información a agregar
+let personas = ["HOMERO","MARGE", "BART", "LISA","MAGGIE"];
+//Iteramos el array con for...of
+for (const persona of personas) {
+    //Creamos un nodo <li> y agregamos al padre en cada ciclo
+    let li = document.createElement("li");
+    li.innerHTML = persona
+    padre.appendChild(li);
+}
